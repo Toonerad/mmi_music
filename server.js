568da928699209d4078e911cfc37bde5102808bc
Toonerad <toonerad@hotmail.com>
2018-01-17 17:39:56 +0100

    Update server.js

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ const commands = {
 
 client.on('ready', () => {
 	console.log('ready!');
-	client.user.setPresence({ game: { name: "Test", type: 0}});
+	client.user.setPresence({ game: { name: "Dev: Lucas", type: 0}});
 });
 
 client.on('message', msg => {
